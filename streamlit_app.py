fdcaf6421007ce5c9675ef41625039ba637641ec
Yun Mao <94030617+sfc-gh-ymao@users.noreply.github.com>
2023-06-12 21:04:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,13 +52,7 @@ except URLError as e:
 # don't run anything past here while we troubleshoot
 streamlit.stop()
     
-    
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
+   
 streamlit.header("The food load list contains:")
 # Snowflake-related functions
 def get_fruit_load_list():
